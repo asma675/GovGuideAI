b0ac62c59dd98d9cb16ff49bd88b66b2302db111
Asma <158836479+asma675@users.noreply.github.com>
2025-12-01 19:05:07 -0500

    Delete utils directory

--- utils/tts.js
+++
@@ -1,8 +0,0 @@
-// utils/tts.js
-export function speak(text, lang = "en") {
-  if (typeof window === "undefined") return;
-
-  const msg = new SpeechSynthesisUtterance(text);
-  msg.lang = lang === "fr" ? "fr-FR" : "en-US";
-  speechSynthesis.speak(msg);
-}
