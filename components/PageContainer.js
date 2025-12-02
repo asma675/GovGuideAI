31cd00332f16d166ad78664e4191432795f914c1
Asma <158836479+asma675@users.noreply.github.com>
2025-12-01 19:02:48 -0500

    Delete components directory

--- components/PageContainer.js
+++
@@ -1,8 +0,0 @@
-export default function PageContainer({ title, children }) {
-  return (
-    <div className="page-container">
-      {title && <h1 className="page-title">{title}</h1>}
-      <div className="page-content">{children}</div>
-    </div>
-  );
-}
